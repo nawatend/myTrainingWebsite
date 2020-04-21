381f3d6e06453e314c883676eca0e98af54116a3
nawatend <32384185+nawatend@users.noreply.github.com>
2020-04-21 21:36:22 +0200

    Updates

---
+++ precache-manifest.35ac02432f67ec2d242fe8856ae230d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600b412bb3632754ac9b922a740ffcd5",
+    "url": "/nawatend/my_training_website/index.html"
+  },
+  {
+    "revision": "fb760db483563801239c",
+    "url": "/nawatend/my_training_website/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "6e2bfd6781ea7a8920ab",
+    "url": "/nawatend/my_training_website/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "fb760db483563801239c",
+    "url": "/nawatend/my_training_website/static/js/2.2ef691d4.chunk.js"
+  },
+  {
+    "revision": "552899cc0fbd34d2e9ca6a4477746e39",
+    "url": "/nawatend/my_training_website/static/js/2.2ef691d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2bfd6781ea7a8920ab",
+    "url": "/nawatend/my_training_website/static/js/main.d783da32.chunk.js"
+  },
+  {
+    "revision": "79c7593f72514f6c60ad",
+    "url": "/nawatend/my_training_website/static/js/runtime-main.4b169714.js"
+  }
+]);
